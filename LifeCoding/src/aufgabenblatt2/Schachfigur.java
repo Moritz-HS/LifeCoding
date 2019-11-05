ca9701320d117f15b5cc6105b8a43cf4b34d8fb8
Moritz-HS <48474933+Moritz-HS@users.noreply.github.com>
2019-11-05 16:36:28 +0100

    test

---
+++ LifeCoding/src/aufgabenblatt2/Schachfigur.java
@@ -0,0 +1,21 @@
+package aufgabenblatt2;
+
+public class Schachfigur {
+private FarbEnum farbe;
+private FigurEnum figurentyp;
+
+public Schachfigur(String farbe, String figurentyp) {
+	
+}
+	
+	
+ private enum FarbEnum{
+	 WEISS,SCHWARZ;
+ }
+ 
+ private enum FigurEnum{
+	 BAUER,TURM,PFERD,BISCHHOF,DAME,KOENIG
+ }
+ 
+ 
+}
