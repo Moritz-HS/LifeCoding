5d565444624028d0e651ac94099f1e6868589314
Moritz-HS <48474933+Moritz-HS@users.noreply.github.com>
2019-12-05 20:26:44 +0100

    Update TreeSetsTest.java

--- src/treeSet/TreeSetsTest.java
+++ src/treeSet/TreeSetsTest.java
@@ -1,39 +1,38 @@
 package treeSet;
 import java.util.TreeSet;
+import java.util.ArrayList;
 
 public class TreeSetsTest {
 
-	
+
 	public static void main(String[] args) {
-		Mitarbeiter m1 = new Mitarbeiter("Dieter", 0001);
-		Mitarbeiter m2 = new Mitarbeiter("Hans-Meier",2023489);
-		Mitarbeiter m3 = new Mitarbeiter("Violetta",1232);
-		Mitarbeiter m4 = new Mitarbeiter("Hand-Maria",23212);
-		Mitarbeiter m5 = new Mitarbeiter("Elke",9232);
-		Mitarbeiter m6 = new Mitarbeiter("Lisa",231);
-		
+
+		ArrayList<Mitarbeiter> arbeiter = new ArrayList<>();
+
+		arbeiter.add(new Mitarbeiter("Dieter", 0001));
+		arbeiter.add(new Mitarbeiter("Hans-Meier",2023489));
+		arbeiter.add(new Mitarbeiter("Violetta",1232));
+		arbeiter.add(new Mitarbeiter("Hand-Maria",23212));
+		arbeiter.add(new Mitarbeiter("Elke",9232));
+		arbeiter.add(new Mitarbeiter("Lisa",231));
+
+
 		TreeSet<Mitarbeiter> tree1 = new TreeSet<Mitarbeiter>(new NameKomperator());
-		tree1.add(m1);
-		tree1.add(m2);
-		tree1.add(m3);
-		tree1.add(m4);
-		tree1.add(m5);
-		tree1.add(m6);
+		for(Mitarbeiter m : arbeiter) {
+			tree1.add(m);
+		}
 		
 		TreeSet<Mitarbeiter> tree2 = new TreeSet<Mitarbeiter>(new PersonalnummerKomperator());
-		tree2.add(m1);
-		tree2.add(m2);
-		tree2.add(m3);
-		tree2.add(m4);
-		tree2.add(m5);
-		tree2.add(m6);
-		
+		for(Mitarbeiter m : arbeiter) {
+			tree2.add(m);
+		}
+
 		for(Mitarbeiter m : tree1) {
 			System.out.print(m.getName()+" ,");
 		}
-		
+
 		System.out.println("");
-		
+
 		for(Mitarbeiter m : tree2) {
 			System.out.print(m.getName()+" ,");
 		}
